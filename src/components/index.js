8193a8555db0fedfb6c2ff353c1e4f3d8a4bd4d2
Picodes10 <biswalsushree000@gmail.com>
2023-11-15 12:35:37 +0530

    Start

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import pattern from "../assets/bg-pattern-desktop.png";
+import big from "../assets/desktop.png";
+import sm from "../assets/mobile.png";
+import arrow from "../assets/icon-arrow.svg";
+import error from "../assets/icon-error.svg";
+import logo from "../assets/logo.svg";
+import mentoricon from "../assets/mentoricon.png";
+
+export default {
+  pattern,
+  big,
+  sm,
+  arrow,
+  error,
+  logo,
+  mentoricon,
+};
